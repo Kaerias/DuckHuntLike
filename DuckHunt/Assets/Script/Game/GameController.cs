3e20a499b8e74905396a6af98f1bc13282aafe9e
Guillaume RAULT <guillaume.rault@epitech.eu>
2017-12-03 19:40:26 +0100

    FINISH

---
+++ DuckHunt/Assets/Script/Game/GameController.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Text Score;
+    public Text Life;
+    public GameObject buttonEndGame;
+    public GameObject buttonRetry;
+    public GameObject sprite;
+    public GameObject BloodParticle;
+
+    public float speed = 1;
+    public float speedUp = 0.5f;
+    public int countLife = 3;
+
+    public AudioClip[] Music;
+
+    GameObject BloodDelete;
+    GameObject invokeFairy;
+    GameObject Fairy;
+    GameObject[] allFairy;
+
+    AudioSource m_MyAudioSource;
+
+    int i = 0;
+    int countScore = 0;
+    bool shootFail = false;
+    bool End = false;
+    bool isActive = false;
+    bool isPause = false;
+
+    private void Start()
+    {
+        m_MyAudioSource = GetComponent<AudioSource>();
+        InvokeFairyOnBoard();
+        Score.text = "Score = " + countScore.ToString();
+        Life.text = "Life = " + countLife.ToString();
+    }
+
+    void Update()
+    {
+        if (!isPause)
+        {
+            if (!End)
+            {
+                Fairy = GameObject.FindGameObjectWithTag("Fairy");
+                SpawnFairy();
+                LifeScore();
+                DisplayScore();
+            }
+            else
+            {
+                if (!isActive)
+                {
+                    StatusButton(true);
+                    isActive = !isActive;
+                }
+            }
+        }
+        ExitGameWithButton();
+    }
+
+    void SpawnFairy()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            InvokeFairyOnBoard();
+        }
+        if (Fairy.GetComponent<ShootFairy>().GetIsShooted())
+        {
+            BloodDelete = Instantiate(BloodParticle, Fairy.transform.position, transform.rotation) as GameObject;
+            BloodDelete.transform.position = Fairy.transform.position;
+            m_MyAudioSource.PlayOneShot(Music[0], 0.7f);
+            InvokeFairyOnBoard();
+            i++;
+            Fairy.GetComponent<ShootFairy>().SetIsShooted();
+            countScore += 1;
+            speed += speedUp;
+            Destroy(BloodDelete, 2f);
+        }
+    }
+
+    public void LifeScore()
+    {
+        if (shootFail)
+        {
+            shootFail = !shootFail;
+            countLife -= 1;
+            m_MyAudioSource.PlayOneShot(Music[1], 0.7f);
+            if (countLife == 0)
+            {
+                Destroy(Fairy);
+                End = true;
+            }
+        }
+    }
+
+    public void Retry()
+    {
+        Fairy.GetComponent<ShootFairy>().SetPause(false);
+        isPause = false;
+        allFairy = GameObject.FindGameObjectsWithTag("Fairy");
+        foreach (GameObject Fairy in allFairy)
+        {
+            Destroy(Fairy);
+        }
+        countLife = 3;
+        countScore = 0;
+        speed = 1;
+        isActive = !isActive;
+        End = false;
+        StatusButton(false);
+        InvokeFairyOnBoard();
+    }
+
+    void InvokeFairyOnBoard()
+    {
+        invokeFairy = Instantiate(sprite, transform.position, transform.rotation) as GameObject;
+        invokeFairy.transform.position = new Vector3(-8, Random.Range(-3.5f, 3.5f), -1);
+    }
+
+    void DisplayScore()
+    {
+        Score.text = "Score = " + countScore.ToString();
+        Life.text = "Life = " + countLife.ToString();
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    void ExitGameWithButton()
+    {
+        if (countLife != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!isPause)
+                {
+                    isPause = true;
+                    Fairy.GetComponent<ShootFairy>().SetPause(true);
+                    StatusButton(true);
+                }
+                else
+                {
+                    isPause = !isPause;
+                    Fairy.GetComponent<ShootFairy>().SetPause(false);
+                    StatusButton(false);
+                }
+            }
+        }
+    }
+
+    void StatusButton(bool b)
+    {
+        buttonEndGame.SetActive(b);
+        buttonRetry.SetActive(b);
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public void SetShootFail(bool b)
+    {
+        shootFail = b;
+    }
+
+}
---
+++ DuckHunt/Assets/Script/Game/ShootFairy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootFairy : MonoBehaviour {
+
+    public GameObject sprite;
+    private GameObject gameBoard;
+
+    public int countLife = 3;
+    bool isShooted = false;
+    float speed;
+    int direction;
+    bool isPause = false;
+
+    private void Start()
+    {
+        gameBoard = GameObject.Find("GameBoard");
+        speed = gameBoard.GetComponent<GameController>().GetSpeed();
+        direction = -1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isPause)
+        {
+            SetDirection();
+            Shooted();
+            Translated();
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        if (!isPause)
+        {
+            isShooted = true;
+            Destroy(gameObject);
+        }
+    }
+
+    void SetDirection()
+    {
+        if (transform.position.x <= -8 || direction == -1)
+            direction = 0;
+        else if (transform.position.x >= 8)
+            direction = 1;
+    }
+
+    void Translated()
+    {
+        if (direction == 0)
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        else if (direction == 1)
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    void Shooted()
+    {
+        if (!isShooted && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            gameBoard.GetComponent<GameController>().SetShootFail(true);
+        }
+    }
+
+    public bool GetIsShooted()
+    {
+        return isShooted;
+    }
+
+    public void SetIsShooted()
+    {
+        isShooted = false;
+    }
+
+    public void SetPause(bool b)
+    {
+        isPause = b;
+    }
+
+}
